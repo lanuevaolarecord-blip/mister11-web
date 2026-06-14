9db75408ad340f2295827142df3ad557486a2280
jhojan <lanuevaolarecord@gmail.com>
2026-06-14 12:38:34 +0200

    bump: actualizar la versión de la app a v1.1.2 tras compilar y desplegar APK

--- upload-apk.mjs
+++ upload-apk.mjs
@@ -24,7 +24,7 @@ const firebaseConfig = {
 };
 
 // ─── Datos de la versión ─────────────────────────────────────────────────────
-const APP_VERSION    = '1.1.0';
+const APP_VERSION    = '1.1.2';
 const APK_LOCAL_PATH = resolve(
   __dirname,
   'android/app/build/outputs/apk/release/mister11.apk'

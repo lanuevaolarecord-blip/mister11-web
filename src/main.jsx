240a82705883980f6321e4903eaca54496c7bca8
jhojan <lanuevaolarecord@gmail.com>
2026-07-14 20:44:06 +0200

    Fix: StatusBar color nativo Android + safe areas unificadas

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,15 @@ import { ThemeProvider } from './context/ThemeContext.jsx'
 import { PizarraProvider } from './context/PizarraContext.jsx'
 import './index.css'
 import App from './App.jsx'
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { Capacitor } from '@capacitor/core';
+
+if (Capacitor.isNativePlatform()) {
+  StatusBar.setStyle({ style: Style.Dark });
+  StatusBar.setBackgroundColor({ color: '#1a2e1a' });
+  StatusBar.setOverlaysWebView({ overlay: false });
+}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
